651a13ef2d10acc7a4c673001e815d17d2e2dc5a
KingCreatBug <trunghoang.240500@gmail.com>
2024-01-29 22:31:24 +0700

    add

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -228,3 +228,26 @@ window.onload = function () {
         };
     }
 };
+
+// Chuyển tab
+window.addEventListener("template-loaded", () => {
+    const tabsSelector = "prod-tab__item";
+    const contentsSelector = "prod-tab__content";
+
+    const tabActive = `${tabsSelector}--active`;
+    const contentActive = `${contentsSelector}--active`;
+
+    const tabContainers = $$(".js-tabs");
+    tabContainers.forEach((tabContainer) => {
+        const tabs = tabContainer.querySelectorAll(`.${tabsSelector}`);
+        const contents = tabContainer.querySelectorAll(`.${contentsSelector}`);
+        tabs.forEach((tab, index) => {
+            tab.onclick = () => {
+                tabContainer.querySelector(`.${tabActive}`)?.classList.remove(tabActive);
+                tabContainer.querySelector(`.${contentActive}`)?.classList.remove(contentActive);
+                tab.classList.add(tabActive);
+                contents[index].classList.add(contentActive);
+            };
+        });
+    });
+});
